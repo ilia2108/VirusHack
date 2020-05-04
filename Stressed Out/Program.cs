daa75662474794b9af5d23c65da0894b3d43a566
Ilya Ryabukhin <ryabuily@cvut.cz>
2020-05-04 21:45:09 +0300

    Add project files.

---
+++ Stressed Out/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System.Threading.Tasks;
+
+namespace Stressed_Out
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
+                .Create("d81502a3-9729-448d-938b-e8b2dcccd437")
+                .Build();
+
+            String[] scopes = { "https://graph.microsoft.com/calendars.read" };
+            // Create an authentication provider by passing in a client application and graph scopes.
+            DeviceCodeProvider authProvider = new DeviceCodeProvider(publicClientApplication, scopes);
+            // Create a new instance of GraphServiceClient with the authentication provider.
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+           
+
+            var calendars = await graphClient.Users["ilia.ryabukhin@studentpartner.com"].Calendar //  CalendarGroups[""].Calendars
+                .Request()
+                .GetAsync();
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
